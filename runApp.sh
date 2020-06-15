513d282dd23f5184d983e2432dcdf2e1e4b1739b
pgc062020 <66740741+pgc062020@users.noreply.github.com>
2020-06-15 10:41:16 +0200

    Add files via upload

--- runApp.sh
+++ runApp.sh
@@ -3,6 +3,7 @@
 baseDir=$(dirname -- "$(readlink -f -- "$BASH_SOURCE")")
 libdir="$baseDir/libs"
 
+ulimit -c unlimited
 chmod u+x "$baseDir/DailyDesktopWallpaperPlus"
 export LD_LIBRARY_PATH=$libdir
 exec "$baseDir/DailyDesktopWallpaperPlus"
