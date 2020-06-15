8db9005c1595894260cf67f2550a73796e3553b4
pgc062020 <66740741+pgc062020@users.noreply.github.com>
2020-06-15 08:15:58 +0200

    Delete run.sh

--- run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-baseDir=$(dirname -- "$(readlink -f -- "$BASH_SOURCE")")
-libdir="$baseDir/libs"
-
-chmod u+x "$baseDir/DailyDesktopWallpaperPlus"
-export LD_LIBRARY_PATH=$libdir
-exec "$baseDir/DailyDesktopWallpaperPlus"
