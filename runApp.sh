ecdb1a350c28a6bb2c0ba87e772a36b5aae6e254
pgc062020 <66740741+pgc062020@users.noreply.github.com>
2020-06-16 22:05:46 +0200

    Delete runApp.sh

--- runApp.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-baseDir=$(dirname -- "$(readlink -f -- "$BASH_SOURCE")")
-libdir="$baseDir/libs"
-
-ulimit -c unlimited
-chmod u+x "$baseDir/DailyDesktopWallpaperPlus"
-export LD_LIBRARY_PATH=$libdir
-exec "$baseDir/DailyDesktopWallpaperPlus"
